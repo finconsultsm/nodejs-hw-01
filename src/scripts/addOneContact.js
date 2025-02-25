57403d539fb3457b2402c797e41f336616f0ac9b
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-02-25 19:01:50 +0100

    1

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,14 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const addOneContact = async () => {
+  const newContact = createFakeContact();
+
+  const contacts = await readContacts();
+  contacts.push(newContact);
+
+  await writeContacts(contacts);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  const newContacts = Array.from({ length: number }, createFakeContact);
+
+  const contacts = await readContacts();
+  contacts.push(...newContacts);
+
+  await writeContacts(contacts);
+};
 
 generateContacts(5);
